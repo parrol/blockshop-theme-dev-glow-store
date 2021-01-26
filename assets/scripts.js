da011fd33ec83cb86f550bac4706ac8b61313d2c
Carlos Parra <carlos.parra97@gmail.com>
2021-01-26 10:35:15 -0400

    feature: add selected option descriptions
    1. Needs cleaning and documentation.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,5 @@
 /**Change header to sticky*/
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
     console.log("DOM fully loaded and parsed");
     // Get the header
     const announcementElement = document.querySelector("#shopify-section-announcement");
@@ -14,14 +14,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
     const offCanvasViewport = document.querySelector('#off-canvas--viewport');
 
     // When the user scrolls the page with both sidebars closed, execute stickyheader
-    window.onscroll = function() {stickyheader(offCanvasViewport.getAttribute('data-off-canvas--state'))};
+    window.onscroll = function () { stickyheader(offCanvasViewport.getAttribute('data-off-canvas--state')) };
 
     // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
     function stickyheader(state) {
         // Get heights
         //let announcementHeight = window.getComputedStyle(announcementElement, null).getPropertyValue("height");
         let headerHeight = window.getComputedStyle(headerElement, null).getPropertyValue("height");
-        if (state === "closed"){
+        if (state === "closed") {
 
             if (window.pageYOffset > sticky) {
                 //announcementElement.classList.add("section--header-sticky");
@@ -40,13 +40,41 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     // set option description (ON HOVER)
     // sets default description visibility
+    let isOptionSelected = false;
+    let selected_variant = null;
+
+    function setSelectedVisibility(isVisible) {
+        if (isVisible) {
+            console.log("add reveal to selected variant");
+            console.log(selected_variant);
+            selected_variant.classList.add("reveal");
+            selected_variant.classList.remove("hidden");
+        } else {
+            console.log("add hidden to selected variant");
+            selected_variant.classList.add("hidden");
+            selected_variant.classList.remove("reveal");
+        }
+    }
+
+    function hideAll(event) {
+        let default_option_description = event.currentTarget.closest('[name = id ]').children
+
+        Array.prototype.forEach.call(default_option_description, function (child) {
+            if (child.getAttribute("data-js-class") !== ("Radios")){
+                child.classList.add("hidden");
+                child.classList.remove("reveal");
+
+            }
+        })
+    }
+
     function setDefaultVisibility(event, isVisible) {
         let default_option_description = event.currentTarget.closest('[name = id ]').children
 
-        Array.prototype.forEach.call(default_option_description, function(child){
-            if (child.getAttribute("data-value") === "default-option-description"){
+        Array.prototype.forEach.call(default_option_description, function (child) {
+            if (child.getAttribute("data-value") === "default-option-description") {
 
-                if (isVisible){
+                if (isVisible) {
                     child.classList.add("reveal");
                     child.classList.remove("hidden");
                 } else {
@@ -60,32 +88,64 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // shows the hovered option's description
     function showOptionDescription(event) {
 
-        setDefaultVisibility(event, false);
-
         let aria_label = event.currentTarget.getAttribute("aria-label");
         let current_button = document.getElementById("option--" + aria_label);
-        current_button.classList.add("reveal");
-        current_button.classList.remove("hidden");
+
+        if (isOptionSelected == false) {
+            setDefaultVisibility(event, false);
+            current_button.classList.add("reveal");
+            current_button.classList.remove("hidden");
+        } else {
+            setSelectedVisibility(false);
+            current_button.classList.add("reveal");
+            current_button.classList.remove("hidden");
+        }
     }
 
     // hides option description and shows default description
     function hideOptionDescription(event) {
-
-        setDefaultVisibility(event, true);
-
         let aria_label = event.currentTarget.getAttribute("aria-label");
         let current_button = document.getElementById("option--" + aria_label);
-        current_button.classList.add("hidden");
-        current_button.classList.remove("reveal");
+
+        if (isOptionSelected == false) {
+            setDefaultVisibility(event, true);
+
+            console.log(`aria_label: ${aria_label}`);
+            current_button.classList.add("hidden");
+            current_button.classList.remove("reveal");
+        } else {
+            console.log("hide description, option selected true");
+
+            hideAll(event);
+            // current_button.classList.add("hidden");
+            // current_button.classList.remove("reveal");
+            setSelectedVisibility(true);
+        }
+
     }
 
-    function onRadioButtonClicked(radio_button) {
+    function selectOptionDescription(radio_button, name) {
+        console.log("selectOptionDescription");
+        let option_descriptions = radio_button.closest('[name = id ]').children
+
+        Array.prototype.forEach.call(option_descriptions, function (child) {
+            console.log(`name: ${name}`);
+            if (child.getAttribute("data-value") === name) {
+
+                selected_variant = child;
+                console.log(`child`);
+                console.log(child);
+            }
+        });
+    }
+
+    function onRadioButtonClicked(event, radio_button) {
         console.log(radio_button);
 
         // get select element
         let form = radio_button.closest("form");
         let selects = form.getElementsByTagName("select")
-        let select_variant  = selects[0]
+        let select_variant = selects[0]
 
         // get variant name
         let variant = radio_button.getAttribute("aria-label");
@@ -96,21 +156,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
         // select option according to radio button selected
         for (option of options_array) {
             // delete white spaces
-            if (variant === option.innerText.replace(/\s/g, "")){
+            if (variant === option.innerText.replace(/\s/g, "")) {
                 select_variant.value = option.getAttribute("value");
+                selectOptionDescription(radio_button, variant);
+                console.log(`selected_variant: ${selected_variant}`);
+                console.log(selected_variant);
             }
         }
 
-        console.log(select_variant);
+        showOptionDescription(event);
+        isOptionSelected = true;
     }
 
     // get class elements that'll be hovered
     let radios__buttons = document.getElementsByClassName('radios--swatch-button');
     // add event listeners for mouseover and mouseout to each radio button
-    Array.prototype.forEach.call(radios__buttons, function(radio__button){
+    Array.prototype.forEach.call(radios__buttons, function (radio__button) {
         radio__button.addEventListener("mouseover", showOptionDescription);
         radio__button.addEventListener("mouseout", hideOptionDescription);
-        radio__button.addEventListener("click", () => {onRadioButtonClicked(radio__button)});
+        radio__button.addEventListener("click", (event) => { onRadioButtonClicked(event, radio__button) });
 
     });
 
